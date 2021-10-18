0802685e881c7491bfb4a3beb14c3f05a6205738
magus0 <harvestashya@gmail.com>
2021-10-18 01:07:41 -0700

    part 2

--- routes/recipes.js
+++ routes/recipes.js
@@ -8,10 +8,14 @@ router.get("/", function (req, res){
   return res.json({recipeNames});
 });
 
-router.get("/details/:name", function (req, res){  
-  let recipeNames = recipes.recipes.map( e => e.name)
+router.get("/details/:recipe", function (req, res){  
+  let recipe = recipes.recipes.find( e => e.name === req.params.recipe);
+  let details = recipe
+    ? { "ingredients": recipe.ingredients,
+      "numSteps": recipe.instructions.length}
+    : {};
 
-  return res.json({recipeNames});
+  return res.json({details});
 });
 
 module.exports = router;
